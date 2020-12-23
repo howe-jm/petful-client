a5eb1a893b0a5827de662b349a0400b9d4635919
Jason Howe <trisynth@gmail.com>
2020-12-23 16:45:05 -0500

    Initial commit.

---
+++ src/PageNotFound/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PageNotFound() {
+  return (
+    <div className='not-found'>
+      <h2>404? Yep. 404.</h2>
+      <p>(That means there's not a page here, dummy.)</p>
+    </div>
+  );
+}
---
+++ src/root/Root.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import AdoptionPage from '../AdoptionPage/AdoptionPage';
+import LandingPage from '../LandingPage/LandingPage';
+import PageNotFound from '../PageNotFound/PageNotFound';
+
+class Root extends React.Component {
+  renderRoutes() {
+    return (
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/adoption' component={AdoptionPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    );
+  }
+
+  render() {
+    return (
+      <div className='root-main'>
+        <header>
+          <h1>Petful</h1>
+        </header>
+        <main>
+          <h2>Welcome to Petful!</h2>
+          {this.renderRoutes()}
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Root;
